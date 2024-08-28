0f89ed79b53da1a1c1a4bbeb55977c49f3a654fa
Vladimir <djeksaly@gmail.com>
2024-08-28 19:08:00 +0300

    Rename or delete old components

--- src/main/java/com/project/dropping/repo/StatusServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.project.dropping.repo;
-
-import com.project.dropping.model.Status;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StatusServiceInterface extends CrudRepository<Status,Long> {
-}
