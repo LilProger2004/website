a1643343ff5e8bcd658d2c8f0e2f62f9ac7a51cb
Vladimir <djeksaly@gmail.com>
2024-09-01 14:28:26 +0300

    change logic

--- src/main/java/com/project/dropping/controller/ProductController.java
+++ src/main/java/com/project/dropping/controller/ProductController.java
@@ -1,10 +1,6 @@
 package com.project.dropping.controller;
 
-import com.project.dropping.model.Category;
 import com.project.dropping.model.Product;
-import com.project.dropping.repository.CategoryServiceInterface;
-import com.project.dropping.repository.ProductServiceInterface;
-import com.project.dropping.repository.UserServiceInterface;
 import com.project.dropping.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -15,18 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class ProductController {
 
-  final ProductServiceInterface productServiceInterface;
-
-  final UserServiceInterface userServiceInterface;
-
-  final CategoryServiceInterface categoryServiceInterface;
-
   final ProductService productService;
   @GetMapping("/Products")
   public String mainPageOfProduct(Model model) {
-    Iterable<Product> products = productServiceInterface.findAll();
-    Iterable<Category> categories = categoryServiceInterface.findAll();
-    model.addAttribute("categories", categories);
+    Iterable<Product> products = productService.findAll();
     model.addAttribute("products", products);
     return "products-page";
   }
