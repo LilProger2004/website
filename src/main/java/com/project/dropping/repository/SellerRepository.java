124a097fe6524f07310cc3e569c1a5ebf5a1800e
Vladimir <djeksaly@gmail.com>
2024-11-10 13:32:22 +0300

    add some methods to repository with transaction annotation

--- src/main/java/com/project/dropping/repository/SellerRepository.java
+++ src/main/java/com/project/dropping/repository/SellerRepository.java
@@ -2,8 +2,16 @@ package com.project.dropping.repository;
 
 import com.project.dropping.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+    @Transactional
+    Seller findBySellerNameIgnoreCase(@NonNull String sellerName);
+
+    @Transactional
+    boolean existsBySellerId(String sellerId);
 }
