168c85156c769ebbfce222288e84f11befb18993
Vladimir <djeksaly@gmail.com>
2024-11-10 13:32:50 +0300

    add some methods and realize

--- src/main/java/com/project/dropping/services/SellerService.java
+++ src/main/java/com/project/dropping/services/SellerService.java
@@ -1,4 +1,11 @@
 package com.project.dropping.services;
 
+import com.project.dropping.model.Seller;
+
+
 public interface SellerService {
+
+    Seller findSellerByName(String sellerName);
+
+    void saveSeller(Seller seller);
 }
--- src/main/java/com/project/dropping/services/impliments/SellerServiceImplements.java
+++ src/main/java/com/project/dropping/services/impliments/SellerServiceImplements.java
@@ -1,6 +1,25 @@
 package com.project.dropping.services.impliments;
 
+import com.project.dropping.model.Seller;
+import com.project.dropping.repository.SellerRepository;
 import com.project.dropping.services.SellerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class SellerServiceImplements implements SellerService {
+
+    final SellerRepository sellerRepository;
+
+    @Override
+    public Seller findSellerByName(String sellerName) {
+        return sellerRepository.findBySellerNameIgnoreCase(sellerName);
+    }
+
+    @Override
+    public void saveSeller(Seller seller) {
+        sellerRepository.save(seller);
+        sellerRepository.existsBySellerId(seller.getSellerId());
+    }
 }
